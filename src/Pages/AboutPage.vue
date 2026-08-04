e7c2a3098f00e6f78136c11ddabb3500aa9a4f2b
Nabila-salsabiya <salsabiyawak18@icloud.com>
2026-08-04 10:28:13 +0700

    first commit

---
+++ src/Pages/AboutPage.vue
@@ -0,0 +1,6 @@
+<template>
+    <main>
+    <h1>bebass</h1>
+    <p>produk baguss</p>
+     </main>
+    </template>
---
+++ src/Pages/LandingPage.vue
@@ -0,0 +1,8 @@
+<template>
+    <main>
+    <div class="main">
+    <h1>Selamat datang di onlineshop akuUuu</h1>
+    <p>Belanja gampang, mudah, dan senang.</p>
+    </div>
+     </main>
+    </template>
